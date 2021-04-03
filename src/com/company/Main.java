559092bc58686fd6d75eead4363b31d59dad602c
Muhsin Khan Kamal ahamed <muhsinkhan@Muhsins-MacBook-Pro.local>
2021-04-03 12:47:18 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringProblems stringProblems = new StringProblems();
+        stringProblems.testMethod();
+    }
+}
---
+++ src/com/company/StringProblems.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class StringProblems {
+    public void testMethod(){
+        System.out.println("This class is String Problems");
+    }
+}
