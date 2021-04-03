b15daa0258353601413876800f8b117e5c1c817b
Muhsin Khan Kamal ahamed <muhsinkhan@Muhsins-MacBook-Pro.local>
2021-04-03 13:13:05 -0500

    development completed

--- src/com/company/StringProblems.java
+++ src/com/company/StringProblems.java
@@ -1,7 +1,31 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class StringProblems {
     public void testMethod(){
         System.out.println("This class is String Problems");
+    }
+
+    /**
+     * This method helps us to find the duplicate characters from string
+     * @param str
+     */
+    public void duplicateCharacters(String str){
+        char c[] =  str.toCharArray();
+        HashSet<Character> characters = new HashSet<Character>();
+        ArrayList duplicateCharacters = new ArrayList();
+        for(int i=0;i<c.length;i++){
+            if(!duplicateCharacters.contains(c[i])){
+                if(!characters.add(c[i])){
+                    duplicateCharacters.add(c[i]);
+                }
+            }
+        }
+        for (int i=0;i<duplicateCharacters.size();i++){
+            System.out.println("Duplicate Numbers are : " + duplicateCharacters.get(i));
+        }
     }
 }
